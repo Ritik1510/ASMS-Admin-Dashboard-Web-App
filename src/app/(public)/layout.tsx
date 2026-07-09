1e053a89f5b9d1d299968e79907b4830aa40c52d
ritik1510 <kr.ritik1510@gmail.com>
2026-07-10 05:17:17 +0530

    applying font to the marketing layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,12 @@
-import MarketingFooter from '@/components/shared/marketing/footer'
-import MarketingHeader from '@/components/shared/marketing/header'
+import MarketingFooter from '@/components/layouts/marketing/footer'
+import MarketingHeader from '@/components/layouts/marketing/header'
 import React from 'react'
 
 export default function MarketingLayout({ children }: Readonly<{children: React.ReactNode }>) {
   return (
-    <div className='grid'>
+    <div className='font-marketing grid'>
       <MarketingHeader />
-      <main className='h-screen w-screen'>{children}</main>
+      <main className='grid'>{children}</main>
       <MarketingFooter />
     </div>
   )
