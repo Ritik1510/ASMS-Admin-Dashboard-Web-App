7f05da009481980c065f2c4409336e9a7ca0ff18
ritik1510 <kr.ritik1510@gmail.com>
2026-08-15 10:13:07 +0530

    just testing the token setup

--- src/dal/complaints/query.dal.ts
+++ src/dal/complaints/query.dal.ts
@@ -3,12 +3,13 @@ import { request } from "@/network/http/client";
 import { useAuth } from "@clerk/nextjs"; // client hook for token extraction
 import { auth } from "@clerk/nextjs/server"; // server-side auth for token extraction
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  role: string;
-}
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+//   role: string;
+// }
+
 async function getTokenFromClerk() {
   const { getToken } = useAuth()
   const token = await getToken()
@@ -16,10 +17,10 @@ async function getTokenFromClerk() {
   return token;
 }
 
-export async function getUsers(token: string | null) {
+export async function getComplaints(token: string | null) {
   // const token = await getTokenFromClerk();
-  console.log("Token in getUsers function:", token);
-  return request<User[]>("/api/v1/users",{
+  console.log("Token in getComplaints function:", token);
+  return request("/api/v1/complaints/get",{
     method: "GET",
     tokenProvider: clerkServerTokenProvider, // Use the server-side token provider
     query: {
