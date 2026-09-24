9a9067001fd9aa00f0c8c4041af2516948319f7e
ritik1510 <kr.ritik1510@gmail.com>
2026-09-24 09:33:19 +0530

    Silly Mistake

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,7 +3,7 @@ import MarketingHeader from '@/components/layouts/marketing/header.layout'
 import { ScrollToTop } from '@/components/ui/scrollToTop'
 import React from 'react'
 
-export default function MarketingLayout({ children }: Readonly<{children: React.ReactNode }>) {
+export default async function MarketingLayout({ children }: Readonly<{children: React.ReactNode }>) {
   return (
     <div className='font-marketing grid relative'>
       <MarketingHeader />
